d1a2adff4125e4e62e57fe4a02ac4fb7dafce2ea
Jose Diego Cardona <TuCorreo>
2024-12-13 23:59:35 -0500

    hoy

---
+++ Gatewaway/src/main/java/com/Gatewaway/GatewawayApplication.java
@@ -0,0 +1,13 @@
+package com.Gatewaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewawayApplication.class, args);
+	}
+
+}
---
+++ Gatewaway/src/test/java/com/Gatewaway/GatewawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gatewaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
